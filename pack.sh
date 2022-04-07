3bfe39fe81e0f750a135003da3bad1639c37c639
arch <arch>
2022-04-07 09:21:38 +0200

    switching from 7z to zip format due to p7zip package not preinstalled on archiso

--- pack.sh
+++ pack.sh
@@ -4,6 +4,6 @@ set -uo pipefail
 set +o history
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
-7z a -up0q0r2x1y2z1w2 packer.7z *.json bootstrap* userbase* package* pipeline.sh pack.sh
-md5sum *.json bootstrap* userbase* package* pipeline.sh pack.sh packer.7z > packer.md5.txt
-sha1sum *.json bootstrap* userbase* package* pipeline.sh pack.sh packer.7z > packer.sha1.txt
+7z a -up0q0r2x1y2z1w2 packer.zip *.json bootstrap* userbase* package* pipeline.sh pack.sh
+md5sum *.json bootstrap* userbase* package* pipeline.sh pack.sh packer.zip > packer.md5.txt
+sha1sum *.json bootstrap* userbase* package* pipeline.sh pack.sh packer.zip > packer.sha1.txt
