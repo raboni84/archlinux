139762170b51fa4df1bb7ae5b48ea202c3ff1a26
arch <arch>
2022-06-15 03:34:45 +0200

    zip itself doesn't need to be in the checksums

--- pack.sh
+++ pack.sh
@@ -5,5 +5,5 @@ set +o history
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 7z a -up0q0r2x1y2z1w2 packer.zip *.json bootstrap* userbase* package* rescue* localmirror* pxeserver* pipeline.sh pack.sh README.md LICENSE
-md5sum *.json bootstrap* userbase* package* rescue* localmirror* pxeserver* pipeline.sh pack.sh packer.zip README.md LICENSE > packer.md5.txt
-sha1sum *.json bootstrap* userbase* package* rescue* localmirror* pxeserver* pipeline.sh pack.sh packer.zip README.md LICENSE > packer.sha1.txt
+md5sum *.json bootstrap* userbase* package* rescue* localmirror* pxeserver* pipeline.sh pack.sh README.md LICENSE > packer.md5.txt
+sha1sum *.json bootstrap* userbase* package* rescue* localmirror* pxeserver* pipeline.sh pack.sh README.md LICENSE > packer.sha1.txt
